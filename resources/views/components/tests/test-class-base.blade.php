7e8f943cfc404a6e13fcea7dcf84d5fe4f112699
nishimurasyota <syota4723@gmail.com>
2022-04-12 18:30:06 +0900

    :sparkles: クラスベースのコンポーネントの使い方

---
+++ resources/views/components/tests/test-class-base.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネント
+    <div>
+        {{$classBaseMessage}}
+    </div>
+    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,8 @@
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="TITLE" />
+    <x-tests.card title="CSS変更" />
+
+    <x-tests.test-class-base classBaseMessage="メッセージです" />
 
 </x-tests.app>
