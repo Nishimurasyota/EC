c4d7573aa171d8dd565e42becc7f68c5f4eb9ce5
nishimurasyota <syota4723@gmail.com>
2022-04-12 13:34:59 +0900

    :sparkles: propsを使用した初期値の設定

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    "title" =>"初期タイトル",
+    'content'=> "初期本文",
+    "message" => "初期メッセージ",
+    ])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{$title}}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,6 +3,6 @@
     component-test1
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
-
+    <x-tests.card title="TITLE" />
 
 </x-tests.app>
