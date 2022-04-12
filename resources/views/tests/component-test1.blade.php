84736b142029cc278624b538aa7756204af767d3
nishimurasyota <syota4723@gmail.com>
2022-04-12 13:02:51 +0900

    :sparkles: 名前付きslotの確認

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">Header1</x-slot>
     component-test1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">Header2</x-slot>
     component-test2
 </x-tests.app>
