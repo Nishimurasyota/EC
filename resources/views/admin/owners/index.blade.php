2de2d80076bfeea2b365c16494a49e9c97b61319
nishimurasyota <syota4723@gmail.com>
2022-04-18 16:58:19 +0900

    :sparkles: Shopモデルを作成

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -25,7 +25,7 @@
                                         <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
                                         <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                                         <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
-                                    </tr>md:
+                                    </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($owners as $owner)
