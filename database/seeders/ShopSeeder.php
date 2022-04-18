aa5f35f2f0bcdcd46ed0b38bd101c6da0cbe1609
nishimurasyota <syota4723@gmail.com>
2022-04-18 17:13:58 +0900

    :seedling: shopのseedを実行

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('shops')->insert([
+            [
+                "owner_id" => 1,
+                "name" => "○○○店",
+                "information" => "ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。",
+                "filename" => "",
+                "is_selling" => true,
+            ],
+            [
+                "owner_id" => 2,
+                "name" => "○○○店",
+                "information" => "ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。",
+                "filename" => "",
+                "is_selling" => true,
+            ],
+        ]);
+    }
+}
