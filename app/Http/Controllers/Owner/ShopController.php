3b8e6741f3f07910f86ca229b67f197f50542590
nishimurasyota <syota4723@gmail.com>
2022-05-14 15:56:38 +0900

    :sparkles: 店舗情報の更新処理で画像アップロードのバリデーションをカスタムリクエストで実装

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UploadImageRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
@@ -41,7 +42,7 @@ class ShopController extends Controller
         return view("owner.shops.edit", compact("shop"));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
