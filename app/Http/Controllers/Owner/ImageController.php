c7e83c265e6e493c548f41959d6ec673f08900f3
Syota Nishimura <syota4723@gmail.com>
2022-06-09 20:42:42 +0900

    :sparkles: Imageの登録処理を実装

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,7 @@ use App\Models\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ImageController extends Controller
 {
@@ -38,7 +39,7 @@ class ImageController extends Controller
         $images = Image::where("owner_id", Auth::id())
         ->orderBy("updated_at","desc")
         ->paginate(20);
-
+        
         return view("owner.images.index", compact("images"));
     }
 
@@ -60,7 +61,23 @@ class ImageController extends Controller
      */
     public function store(UploadImageRequest $request)
     {
-        dd($request);
+        $imageFiles = $request->file("files");
+        if(is_array($imageFiles)){
+            foreach($imageFiles as $imageFile){
+                $fileNameToStore = ImageService::upload($imageFile, "products");
+            Image::create([
+                "owner_id" => Auth::id(),
+                "filename" => $fileNameToStore
+            ]);
+            }
+        }
+
+        return redirect()
+        ->route("owner.images.index")
+        ->with([
+            'message' => "画像登録を実施しました",
+            "status" => "info",
+        ]);;
     }
 
     /**
