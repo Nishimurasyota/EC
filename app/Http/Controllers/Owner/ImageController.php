94f1f2757cf42cd1bb177ad506f0198f6369e6cb
Syota Nishimura <syota4723@gmail.com>
2022-07-03 12:03:37 +0900

    :adhesive_bandage: 商品に紐づく画像が削除された時の処理を実装

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UploadImageRequest;
+use App\Models\Product;
 use App\Services\ImageService;
 use Illuminate\Contracts\Cache\Store;
 
@@ -128,6 +129,34 @@ class ImageController extends Controller
     public function destroy($id)
     {
         $image = Image::findOrFail($id);
+
+        $imageInProducts = Product::where("image1", $image->id)
+        ->orWhere("image2", $image->id)
+        ->orWhere("image3", $image->id)
+        ->orWhere("image4", $image->id)
+        ->get();
+
+        if($imageInProducts){
+            $imageInProducts->each(function($product)use($image){
+                if($product->image1 === $image->id){
+                    $product->image1 = null;
+                    $product->save();
+                }
+                if($product->image2 === $image->id){
+                    $product->image2 = null;
+                    $product->save();
+                }
+                if($product->image3 === $image->id){
+                    $product->image3 = null;
+                    $product->save();
+                }
+                if($product->image4 === $image->id){
+                    $product->image4 = null;
+                    $product->save();
+                }
+            });
+        }
+
         $filepath = "public/products/" . $image->filename;
 
         if(Storage::exists($filepath)){
@@ -135,6 +164,7 @@ class ImageController extends Controller
         }
 
         Image::findOrFail($id)->delete();
+
         return redirect()
             ->route("owner.images.index")
             ->with([
