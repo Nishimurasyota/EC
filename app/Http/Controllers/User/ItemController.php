fd7e0c2fefe0c7c6bd2f847c539f82bd550f8e80
Syota Nishimura <syota4723@gmail.com>
2022-07-20 21:19:44 +0900

    :adhesive_bandage: 検索フォーム、view側の調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\PrimaryCategory;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Http\Request;
@@ -59,7 +60,11 @@ class ItemController extends Controller
         // ,'image1.filename as filename')
         // ->get();
 
-        return view("user.index" ,compact("products"));
+        $categories = PrimaryCategory::with("secondary")->get();
+
+        // dd($request);
+
+        return view("user.index" ,compact("products","categories"));
     }
 
     public function show($id){
